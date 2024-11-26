95d1e9ed670b80e16059796a75955fc9f9d610a1
sososo <c0b231154e@edu.teu.ac.jp>
2024-11-26 14:25:28 +0900

    爆弾生成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -12,6 +13,7 @@ DELTA = {
     pg.K_RIGHT:(5, 0),
 }
 
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -22,6 +24,15 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20,20)) #爆弾サーフェイス
+    pg.draw.circle(bb_img,(255, 0, 0),(10,10),10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx,vy = +5,+5 #爆弾の速度ベクトル
+
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -39,7 +50,9 @@ def main():
                 sum_mv[1] += tpl[1]
 
         kk_rct.move_ip(sum_mv)
+        bb_rct.move_ip(vx,vy)
         screen.blit(kk_img, kk_rct)
+        screen.blit(bb_img,bb_rct)
         pg.display.update()
 
         tmr += 1
